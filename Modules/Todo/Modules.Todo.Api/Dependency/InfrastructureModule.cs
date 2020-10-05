2e98573e00754375c7588dfa8f8f617a2faadffc
Nico Sap <nico@sapico.me>
2020-10-06 01:58:34 +0200

    remove redundant files Adjust search button in overview

--- Modules/Todo/Modules.Todo.Api/Dependency/InfrastructureModule.cs
+++ Modules/Todo/Modules.Todo.Api/Dependency/InfrastructureModule.cs
@@ -14,8 +14,8 @@ namespace Modules.Todo.Api.Dependency
     {
         protected override void Load(ContainerBuilder builder)
         {
-            //builder.RegisterModule(new Modules.Todo.Infrastructure.Dependency.InfrastructureModule());
-            builder.RegisterModule(new Modules.Todo.Infrastructure.Sql.Dependency.InfrastructureModule());
+            builder.RegisterModule(new Modules.Todo.Infrastructure.Dependency.InfrastructureModule());
+            //builder.RegisterModule(new Modules.Todo.Infrastructure.Sql.Dependency.InfrastructureModule());
 
         }
 
