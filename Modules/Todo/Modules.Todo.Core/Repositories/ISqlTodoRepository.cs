0a57eab13d07b589a33401946571759eeb8e4234
Nico Sap <nico@sapico.me>
2020-10-06 02:13:59 +0200

    Add example ISqlTodoRepository

---
+++ Modules/Todo/Modules.Todo.Core/Repositories/ISqlTodoRepository.cs
@@ -0,0 +1,28 @@
+﻿//Copyright 2020 - 2020  
+
+namespace Modules.Todo.Core.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using BuildingBlock.Specification;
+    using Modules.Todo.Core.Entities;
+
+    /// <summary>
+    /// Defines the <see cref="ISqlTodoRepository" />.
+    /// </summary>
+    public interface ISqlTodoRepository
+    {
+        Task Delete(Guid id);
+
+        Task<TodoAggregate> Get(Guid id);
+        Task<IReadOnlyList<Modules.Todo.Core.Entities.TodoAggregate>> Get(ISpecification<TodoAggregate, Modules.Todo.Core.Specifications.ITodoSpecificationVisitor> spec);
+        Task Save(TodoAggregate item);
+        Task Delete(TodoAggregate item);
+
+        //Task Update(TodoAggregate item);
+
+        Task<bool> UpdateCompletedState(Guid id, bool completedState);
+        Task<bool> UpdateDescription(Guid id, string description);
+    }
+}
